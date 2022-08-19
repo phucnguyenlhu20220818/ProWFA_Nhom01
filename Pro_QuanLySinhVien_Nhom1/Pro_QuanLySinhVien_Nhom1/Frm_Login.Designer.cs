8ef74f9eb287468418c3c18ab7dc4291fa0042cc
Nguyen Minh Phuc <nmphuc@outlook.com>
2022-08-19 11:07:59 +0700

    Phúc hoàn tất From đăng nhập

--- Pro_QuanLySinhVien_Nhom1/Pro_QuanLySinhVien_Nhom1/Frm_Login.Designer.cs
+++ Pro_QuanLySinhVien_Nhom1/Pro_QuanLySinhVien_Nhom1/Frm_Login.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Pro_QuanLySinhVien_Nhom1
 {
-    partial class Form1
+    partial class Frm_Login
     {
         /// <summary>
         /// Required designer variable.
@@ -32,10 +32,9 @@ namespace Pro_QuanLySinhVien_Nhom1
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Frm_Login";
         }
 
         #endregion
     }
 }
-
